36c8be8a626b35bf83a7ba754bd2d0d5f8cb1ad1
Alicia <a.skogblad@gmail.com>
2025-08-28 17:49:33 +0200

    :fire: removed context

--- src/pages/AnimalApp/AnimalApp.tsx
+++ src/pages/AnimalApp/AnimalApp.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import type { IAnimal } from "../../models/IAnimal";
 import { getAnimals } from "../../services/animalService";
-//import styles from "./AnimalApp.module.scss";
-import { AnimalsContext } from "../../context/AnimalsContext";
 import { AnimalsList } from "../../components/AnimalsList/AnimalsList";
 
 export const AnimalApp = () => {
@@ -10,17 +8,17 @@ export const AnimalApp = () => {
 
   useEffect(() => {
     const getData = async () => {
-      const animal = await getAnimals();
-      setAnimals(animal);
+      const animals = await getAnimals();
+      setAnimals(animals);
     }
 
     getData();
   }, []);
   
   return (
-    <AnimalsContext.Provider value={{ animals, feedAnimal: () => {} }}>
+    <>
       <h2>Djuren hos oss</h2>
-      <AnimalsList />
-    </AnimalsContext.Provider>
+      <AnimalsList animals={animals} />
+    </>
   );
 }
